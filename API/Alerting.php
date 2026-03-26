d05a74422aa8546c3f569bc71c01e9345f9d719c
mateo <m.carvalhodossantos@myskolae.fr>
2026-03-26 17:40:15 +0100

    PATCH

--- API/Alerting.php
+++ API/Alerting.php
@@ -253,7 +253,7 @@ function CreateAlertingDisk($diskUsage, $Hostname) {
             return ($result['data']['id'] and  $severity);
         }
         else {
-            echo "Erreur". $result." ".$httpCodeAlert;
+            echo "Erreur". $AlertingData["application_id"] ." ".$result." ".$httpCodeAlert;
 
         }
 
